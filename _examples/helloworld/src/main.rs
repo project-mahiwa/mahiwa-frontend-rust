6e964b0464051b8cd579253156e3dbf8e9ed8707
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-11-16 17:21:50 +0900

    fix _start関数をexportさせるように変更

--- _examples/helloworld/src/main.rs
+++ _examples/helloworld/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
+#![no_main]
+// mahiwa-backendが認識するWebAssemblyのエントリー関数が_startなのでそれに合わせる
 
 use mahiwa_frontend_rust::arduino;
 use mahiwa_frontend_rust::serial;
 
-fn main() {
+#[no_mangle]
+fn _start() {
     serial::print("hello mahiwa written in Rust");
     loop {
         arduino::delay(900);
