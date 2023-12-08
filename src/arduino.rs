dafaa59c87bfde15eba4e4ab2a50a836761d342c
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-08 16:56:37 +0900

    feat 最低限のコメントを追加

--- src/arduino.rs
+++ src/arduino.rs
@@ -3,6 +3,13 @@ extern "C" {
     #[link_name = "delay"]
     fn _delay(ms: u32);
 }
+/// Equivalent to Arduino's delay
+///
+/// # Examples
+///
+/// ```
+/// arduino::delay(1000);
+/// ```
 pub fn delay(ms: u32) {
     unsafe {
         _delay(ms);
--- src/serial.rs
+++ src/serial.rs
@@ -4,9 +4,17 @@ extern "C" {
     fn _print(utf8: *const u8, len: usize);
 }
 
+/// Equivalent to Arduino's print
+///
+/// # Examples
+///
+/// ```
+/// serial::print("hello mahiwa");
+/// ```
 pub fn print(string: &str) {
     unsafe { _print(string.as_ptr(), string.len()) }
 }
+/// Equivalent to Arduino's println
 pub fn println(string: &str) {
     print(string);
     print("\n");
