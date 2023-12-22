54b7b852f578fe4c9c8bcc541c5d364fc7ae6464
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-22 15:20:25 +0900

    feat primeとoptの設定

---
+++ _benchmarks/prime/src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+// mahiwa-backendが認識するWebAssemblyのエントリー関数が_startなのでそれに合わせる
+use mahiwa_frontend_rust::{arduino, serial};
+
+fn is_prime(num: i32) -> bool {
+    if num <= 1 {
+        return false;
+    }
+    for i in 2..=((num as f64).sqrt() as i32) {
+        if num % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+#[no_mangle]
+fn _start() {
+    arduino::delay(5000);
+    serial::println("Rust mahiwa | check_1s");
+    let start = arduino::micros();
+    let result = is_prime(2147483647);
+    let end = arduino::micros();
+    serial::print("result: ");
+    if result {
+        serial::print("prime");
+    } else {
+        serial::print("not prime");
+    }
+    serial::println("");
+    serial::print("time(micro): ");
+    serial::print_i64(end - start);
+    serial::println("");
+}
